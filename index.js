bf1094031c127e40587205a34df8cd2018f950c5
mallepellyvaishnavi <mallepally.vaishnavi@gmail.com>
2025-03-02 14:03:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const serverless = require('serverless-http')
+const app = express();
+const port = 3000;
+
+// Sample data
+const products = [
+  { id: 1, name: "Apple", price: 1.5 },
+  { id: 2, name: "Banana", price: 0.5 },
+  { id: 3, name: "Orange", price: 1.0 },
+];
+
+// GET all products
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+// GET product by ID
+app.get("/api/products/:id", (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = products.find((p) => p.id === productId);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  res.json(product);
+});
+
+// Start server
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+module.exports.handler = serverless(app);
